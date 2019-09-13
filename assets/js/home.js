6beda83fafe62c83782553dbd087e208f8617091
Karyn Vo <kbvo@uw.edu>
2019-09-12 21:24:04 -0700

    initial commit

---
+++ assets/js/home.js
@@ -0,0 +1,20 @@
+jQuery.noConflict();
+jQuery(document).ready(function($){
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer',
+        // gutter: 10
+    });
+
+    $grid.imagesLoaded().progress(function(){
+        $grid.masonry();
+    });
+
+    $grid.on( 'click', '.grid-item', function() {
+        // change size of item via class
+        $( this ).toggleClass('grid-item--gigante');
+        // trigger layout
+        $grid.masonry();
+    });
+});
